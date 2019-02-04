986bb2574c22e53da2ce86949e59ea3b548037ec
Vyacheslav Fisyuk <bspotf@gmail.com>
2019-02-04 23:45:23 +0300

    main ref

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
 #include <QApplication>
 #include "./forms/mainwindow.h"
 #include "./services/dbservice.h"
-#include "./services/salaryservice.h"
 #include <memory>
-
-#include <QSqlQuery>
-#include <QSqlError>
-#include <QVariant>
-#include <ctime>
 
 int main(int argc, char *argv[])
 {
@@ -16,19 +10,6 @@ int main(int argc, char *argv[])
     std::shared_ptr<DbService> dBService = DbService::getInstance();
     dBService->closeDatabase();
     dBService->openDatabase();
-    (new SalaryService)->getWorkerSalary(6);
-    (new SalaryService)->countSalaryExpences(std::time(0));
-    QString query = "SELECT * FROM worker";
-
-
-    QSqlQuery result = dBService->execute(query);
-    result = dBService->find("worker", "worker.id = 1");
-    while (result.next()) {
-        qDebug("id = %d, text = %s.", result.value(0).toInt(),
-               qPrintable(result.value(1).toString()));
-    }
-
-//    return 0;
 
     MainWindow w;
     w.show();
